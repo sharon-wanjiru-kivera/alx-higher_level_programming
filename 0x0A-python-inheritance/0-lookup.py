2810705bcc19acdc7dfde19d143cea123d406c16
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-10-26 01:41:53 -0700

    add file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,8 +4,6 @@
 Contains the lookup function
 =============================
 """
-
-
 def lookup(obj):
      """returns a list of available attributes and methods of an object"""
      return dir(obj)
